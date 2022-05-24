ab52610e327c50770d4a797ad4d52bbfc162b16f
C A <consolation.archaism@protonmail.com>
2022-05-24 23:21:54 +0100

    add basic eval, literal type

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,8 @@ import Text.Parsec
 import Text.Parsec.Char (alphaNum)
 import Text.Parsec.String (Parser, GenParser)
 
-data Exp = SExp String [Exp]
-  | IntItem Int
-  | StringItem String
-  | BoolItem Bool deriving Show
+data VariableValue = I Int | S String | B Bool deriving Show
+data Exp = SExp String [Exp] | Literal VariableValue deriving Show
 
 whitespace :: GenParser Char () String
 whitespace = string " " <|> string "\n" <|> string "\t"
@@ -31,16 +29,22 @@ int = read <$> many1 digit
 bool :: GenParser Char () Bool
 bool = True <$ string "true" <|> False <$ string "false"
 
+literal :: GenParser Char () VariableValue
+literal = I <$> int
+  <|> B <$> bool
+  <|> S <$> many letter
+
 expression :: GenParser Char () Exp
 expression = between (string "(") (string ")") sexp
-  <|> IntItem <$> int
-  <|> BoolItem <$> bool
-  <|> StringItem <$> stringValue
+  <|> Literal <$> literal
 
-prompt :: String -> IO String
-prompt promptText = do
-  putStrLn promptText
-  getLine
+apply :: String -> [VariableValue] -> VariableValue
+apply "add" [I v1, I v2] = I (v1 + v2)
+apply _ _ = I 0
+
+eval :: Exp -> VariableValue
+eval (Literal v) = v
+eval (SExp funcName args) = apply funcName $ map eval args
 
 loop :: IO () -> IO ()
 loop action = do
@@ -50,5 +54,8 @@ loop action = do
 main :: IO ()
 main = do
   loop $ do
-    input <- prompt "Enter a LISP expression:"
-    parseTest expression input
+    putStrLn "Enter a LISP expression:"
+    input <- getLine
+    case parse expression "" input of
+      Left pe -> print pe
+      Right exp -> print $ eval exp
