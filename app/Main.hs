f88af863307d1c18bcc9cbe35d24d85f75d68311
C A <consolation.archaism@protonmail.com>
2022-05-25 14:26:22 +0100

    record sourcepos for some tokens

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,18 @@ import Text.Parsec
 import Text.Parsec.Char (alphaNum)
 import Text.Parsec.String (Parser, GenParser)
 
+data Token = LParen
+  | RParen
+  | Whitespace
+  | Symbol String deriving Show
+
 data VariableValue = I Int | S String | B Bool deriving Show
-data Exp = SExp String [Exp] | Literal VariableValue deriving Show
+type Pos = (SourcePos, SourcePos)
+data FuncName = FuncName Pos String deriving Show
+data Exp = SExp Pos FuncName [Exp] | Literal Pos VariableValue deriving Show
+
+getSourcePos :: Monad m => ParsecT s u m SourcePos
+getSourcePos = fmap statePos getParserState
 
 
 whitespace :: Parsec String () String
@@ -19,10 +29,24 @@ sexpBody = do
   option [] whitespace
   sepBy expression whitespace
 
+funcName :: Parsec String () FuncName
+funcName = do
+  funcNameStartPos <- getSourcePos
+  funcNameText <- stringValue
+  funcNameEndPos <- getSourcePos
+  return $ FuncName (funcNameStartPos, funcNameEndPos) funcNameText
+
 sexp :: Parsec String () Exp
 sexp = do
-  funcName <- stringValue
-  SExp funcName <$> sexpBody
+  string "("
+  sexpStartPos <- getSourcePos
+  option [] whitespace
+  funcNameObj <- funcName
+  option [] whitespace
+  sexpBodyObj <- sexpBody
+  string ")"
+  sexpEndPos <- getSourcePos
+  return $ SExp (sexpStartPos, sexpEndPos) funcNameObj sexpBodyObj
 
 int :: Parsec String () Int
 int = read <$> many1 digit
@@ -30,22 +54,26 @@ int = read <$> many1 digit
 bool :: Parsec String () Bool
 bool = True <$ string "true" <|> False <$ string "false"
 
-literal :: Parsec String () VariableValue
-literal = I <$> int
-  <|> B <$> bool
-  <|> S <$> many letter
+literal :: Parsec String () Exp
+literal = do
+  startPos <- getSourcePos
+  value <- I <$> int <|> B <$> bool <|> S <$> many letter
+  endPos <- getSourcePos
+  return $ Literal (startPos, endPos) value
 
 expression :: Parsec String () Exp
-expression = between (string "(") (string ")") sexp
-  <|> Literal <$> literal
+expression = sexp <|> literal
 
-apply :: String -> [VariableValue] -> VariableValue
-apply "add" [I v1, I v2] = I (v1 + v2)
-apply _ _ = I 0
+apply :: String -> [VariableValue] -> Maybe VariableValue
+apply "add" [I v1, I v2] = Just $ I (v1 + v2)
+apply _ _ = Nothing
 
 eval :: Exp -> VariableValue
-eval (Literal v) = v
-eval (SExp funcName args) = apply funcName $ map eval args
+eval (Literal _ v) = v
+eval (SExp _ (FuncName _ funcName) args) = case apply funcName $ map eval args of
+  Nothing -> I 0
+  Just vv -> vv
+
 
 loop :: IO () -> IO ()
 loop action = do
@@ -59,4 +87,6 @@ main = do
     input <- getLine
     case parse expression "" input of
       Left pe -> print pe
-      Right exp -> print $ eval exp
+      Right exp -> do
+        print exp
+        print $ eval exp
